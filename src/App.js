f3d1a4998e468aa8fbc787543093c61f08979239
Justin Perez <justin@startrev.com>
2019-10-24 17:34:35 -0700

    Minor updates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Dashboard from './components/dashboard';
+
+import config from './components/config';
+
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${ config.spreadsheetId }/values:batchGet?ranges=Sheet1&majorDimension=ROWS&key=${config.apiKey}`;
+
+class App extends Component {
+  render() {
+    return (
+
+      <Dashboard></Dashboard>
+
+    );
+  }
 }
 
 export default App;
