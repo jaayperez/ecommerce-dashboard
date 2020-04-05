80792e6b29149a1d0891d47b18eb91cf78b9199a
Justin Perez <justin@startrev.com>
2020-04-04 22:36:40 -0700

    Update data

--- src/components/chart.js
+++ src/components/chart.js
@@ -8,21 +8,21 @@ function createData(time, amount) {
 }
 
 const data = [
-  createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
-  createData('24:00', undefined),
+  createData('0am', 0),
+  createData('3am', 300),
+  createData('6am', 600),
+  createData('9am', 800),
+  createData('12pm', 1500),
+  createData('3pm', 2000),
+  createData('6pm', 2400),
+  createData('9pm', 2400),
+  createData('12am', undefined),
 ];
 
 export default function Chart() {
   return (
     <React.Fragment>
-      <Title>Today's Summary</Title>
+      <Title>Sales Today</Title>
       <ResponsiveContainer>
         <LineChart
           data={data}
