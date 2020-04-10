8420844465887f1de6dd0ea5937e10a846f2899d
Justin Perez <justin@startrev.com>
2020-04-10 15:51:14 -0700

    Use the Hook API to generate a new style

--- src/components/title.js
+++ src/components/title.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles (theme => ({
+  title: {
+    color: '#444545',
+    textAlign: 'center',
+  },
+}));
 
 export default function Title(props) {
+  const classes = useStyles();
+
   return (
-    <Typography component="h2" variant="h6" color="primary" gutterBottom>
+    <Typography className={classes.title} component="h2" variant="h6" color="primary" gutterBottom>
       {props.children}
     </Typography>
   );
